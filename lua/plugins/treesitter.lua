968328fe06e4d156bbd7cd9d0da96efa0f9cb7de
Cornelioushandsome <145301075+Cornelioushandsome@users.noreply.github.com>
2025-12-22 18:16:38 -0500

    Refactor Treesitter configuration options

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,22 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "python", "javascript", "c", "cpp" , "css" },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+  opts = {
+    ensure_installed = {
+        "lua",
+				"c",
+				"cpp",
+				"bash",
+				"vim",
+				"vimdoc",
+				"luadoc",
+				"rust",
+				"python",
+				"css",
+    },
+    auto_install = true,
+    highlight = { enable = true },
+		indent = { enable = true },
+  },
 }
 
