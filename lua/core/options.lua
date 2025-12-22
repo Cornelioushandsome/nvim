cdda7b95a2959d009a86b755f68639c51fbf6c89
Cornelioushandsome <145301075+Cornelioushandsome@users.noreply.github.com>
2025-12-22 18:18:05 -0500

    Refactor Vim options and key mappings

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,11 +1,38 @@
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.opt.number = true         
-vim.opt.relativenumber = true  
-vim.opt.tabstop = 2          
-vim.opt.shiftwidth = 2         
-vim.opt.expandtab = true       
-vim.opt.smartindent = true    
+
+vim.g.have_nerd_font = true
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.showmode = false
+
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+vim.o.smartindent = true
+vim.o.expandtab = true
+
+vim.schedule(function()
+	vim.o.clipboard = "unnamedplus"
+end)
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+vim.o.confirm = true
+
+vim.o.inccommand = "split"
+
+vim.o.cursorline = true
+
+vim.o.scrolloff = 3
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<CR>", { silent = true })
