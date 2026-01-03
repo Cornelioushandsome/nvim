d6bc102aba0d44e8ff3efe1a54d20623d717fcd4
Cornelioushandsome <145301075+Cornelioushandsome@users.noreply.github.com>
2026-01-03 16:03:46 -0500

    Disable nvim-cmp integration in autopairs.lua
    Comment out nvim-cmp integration for auto-pairing.

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -6,9 +6,9 @@ return {
       fast_wrap = {fast_wrap = { map = '<M-e>' }},       -- optional: allows you to wrap selections quickly
     })   
     -- Optional: integrate with nvim-cmp for auto-completing brackets after selecting a function
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    local cmp = require('cmp')
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    -- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    -- local cmp = require('cmp')
+    -- cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
   end,
 }
 
